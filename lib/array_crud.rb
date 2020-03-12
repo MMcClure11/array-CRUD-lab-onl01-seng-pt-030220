d27fd16a1084dbd3688d45c7b11b04555f68b678
Megan Christine McClure <mcclme02@luther.edu>
2020-03-12 18:02:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -62,6 +62,6 @@ def retrieve_element_from_index(array, i)
   array[i]
 end 
 
-def retrieve_first_element_from_array_element(arr)
-  arr[0]
+def retrieve_first_element_from_array(arr)
+  arr.first 
 end 
